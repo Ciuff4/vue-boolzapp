8160350ec409b4e37033a13e6f74b93b5d0e9865
Ciuff4 <gasperini_cristina@libero.it>
2022-05-06 17:32:16 +0200

    milestone 1

--- script.js
+++ script.js
@@ -163,14 +163,34 @@ const app= new Vue({
                         status: 'received'
                     }
                 ],
+            },{
+                name: 'Davide',
+                avatar: '_8',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Ciao, andiamo a mangiare la pizza stasera?',
+                        status: 'received'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:51:00',
+                        message: 'OK!!',
+                        status: 'received'
+                    }
+                ],
             }
         ],
-        currentChat:0,
+        currentUser: "",
     },
     methods:{
-        // changeImgAvatar(index){
-        //     picture= ;
-        //     console.log(chats[index].avatar);
-        // }
+        changeUser(index){
+            this.currentUser=this.chats[index];
+        }
     }
 })
